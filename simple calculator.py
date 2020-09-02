6cdf70ab5be5df60bc2f277fa35b46ed2cc9364b
Sayyad Shafiyev <sshafiyev@gmail.com>
2020-09-02 13:54:36 +0300

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -2,15 +2,16 @@ a1 = int(input("First digit: "))
 a2 = int(input("Second digit: "))
 z = str(input( "Enter +,-,/,* : "))
 
-if (a1 == 0 or a2 == 0) and z == "/":
-    print("Division by zero is undefined!")
-elif (a1 > 0 and a2 > 0) and z == "+":
+
+if z == "+":
     print(a1 + a2)
-elif (a1 > 0 and a2 > 0) and z == "-":
+elif z == "-":
     print(a1 - a2)
-elif (a1 > 0 and a2 > 0) and z == "*":
+elif z == "*":
     print(a1 * a2)
-elif (a1 > 0 or a2 > 0) and z == "/":
+elif a2 > 0 and z == "/":
     print(int(a1 / a2))
+elif a2 == 0 and z == "/":
+    print("Division by zero is undefined!")
 else:
     print("Wrong operation")
